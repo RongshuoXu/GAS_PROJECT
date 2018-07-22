00aeaad841ffae01dc5cbbd52c35723c212003f1
mzy <Acessss@users.noreply.github.com>
2018-07-22 23:41:39 +0800

    front plungins&pages commit

--- src/main/java/com/springMVC/controller/TreeController.java
+++ src/main/java/com/springMVC/controller/TreeController.java
@@ -4,14 +4,12 @@ import com.springMVC.pojo.TreeInfo;
 import com.springMVC.service.TreeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.List;
+
 
 @Controller
 public class TreeController {
